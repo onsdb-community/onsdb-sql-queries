34d3983f2306dc03599485fe6f915275b5ad8ec0
odion-chantal <158143843+odion-chantal@users.noreply.github.com>
2026-01-30 17:07:10 +0100

    Create test.sql
    testen van PR

---
+++ queries/test.sql
@@ -0,0 +1,25 @@
+/*
+===============================================================================
+Titel       : <korte, duidelijke titel>
+Doel        : <wat levert deze query op en waarom bestaat hij?>
+Auteur      : <naam>
+===============================================================================
+
+Korte uitleg van de logica
+- <bullet 1>
+- <bullet 2>
+- <bullet 3>
+
+*/
+
+
+SELECT
+    -- voorbeeldkolommen
+    t.kolom_1,
+    t.kolom_2
+FROM
+    some_table t
+WHERE
+    t.kolom_1 IS NOT NULL
+ORDER BY
+    t.kolom_1;
